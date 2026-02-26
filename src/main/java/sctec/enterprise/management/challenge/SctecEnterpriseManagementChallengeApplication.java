af4b91736236c0fb7c3c2a8cc96b7706ed38945d
AlissonPeter <126627996+AlissonPeter@users.noreply.github.com>
2026-02-25 21:03:20 -0300

    Initial backend project structure

---
+++ src/main/java/sctec/enterprise/management/challenge/SctecEnterpriseManagementChallengeApplication.java
@@ -0,0 +1,13 @@
+package sctec.enterprise.management.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SctecEnterpriseManagementChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SctecEnterpriseManagementChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sctec/enterprise/management/challenge/SctecEnterpriseManagementChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package sctec.enterprise.management.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SctecEnterpriseManagementChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
