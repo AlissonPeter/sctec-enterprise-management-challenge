128ae1679d5121651d04cfae0d8c4c9bc39e65f6
AlissonPeter <126627996+AlissonPeter@users.noreply.github.com>
2026-03-01 21:56:53 -0300

    Added description in Portuguese to the segments enum.

--- src/main/java/sctec/enterprise/management/challenge/model/enums/Segment.java
+++ src/main/java/sctec/enterprise/management/challenge/model/enums/Segment.java
@@ -1,8 +1,18 @@
 package sctec.enterprise.management.challenge.model.enums;
 public enum Segment {
-    TECHNOLOGY,
-    COMMERCE,
-    INDUSTRY,
-    SERVICES,
-    AGRIBUSINESS
+    TECHNOLOGY("Tecnologia"),
+    COMMERCE("Comércio"),
+    INDUSTRY("Indústria"),
+    SERVICES("Serviços"),
+    AGRIBUSINESS("Agronegócio");
+
+    private final String description;
+
+    Segment(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
