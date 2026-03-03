8f5f43ea9913f1ba647869d5838f079a761791a8
AlissonPeter <126627996+AlissonPeter@users.noreply.github.com>
2026-03-03 10:25:44 -0300

    Added @JsonValue and @JsonCreator to handle localized descriptions of the segment in JSON while preserving constant names in the database.

--- src/main/java/sctec/enterprise/management/challenge/model/enums/Segment.java
+++ src/main/java/sctec/enterprise/management/challenge/model/enums/Segment.java
@@ -1,4 +1,8 @@
 package sctec.enterprise.management.challenge.model.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Segment {
     TECHNOLOGY("Tecnologia"),
     COMMERCE("Comércio"),
@@ -12,7 +16,27 @@ public enum Segment {
         this.description = description;
     }
 
+    /**
+     * Returns the enum description, including the JSON.
+     * @return
+     */
+    @JsonValue
     public String getDescription() {
         return description;
+    }
+
+    /**
+     * Make the POST request accept the enum description coming from a JSON object.
+     * @return
+     */
+    @JsonCreator
+    public static Segment fromDescription(String description) {
+        for (Segment s : Segment.values()) {
+            // Compara ignorando maiúsculas/minúsculas e espaços
+            if (s.description.equalsIgnoreCase(description.trim())) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("Segmento inválido: " + description);
     }
 }
